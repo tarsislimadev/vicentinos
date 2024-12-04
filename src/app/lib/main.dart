44b8fa6ec3b1b772d728a44ad2f18cbf494f94de
Tarsis Lima <br.tmvdl@gmail.com>
2024-12-03 22:43:40 -0300

    Updating app files

--- src/app/lib/main.dart
+++ src/app/lib/main.dart
@@ -14,24 +14,52 @@ class TabBarDemo extends StatelessWidget {
         length: 3,
         child: Scaffold(
           appBar: AppBar(
+            title: const Text('Vicentinos v0.1'),
             bottom: const TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.person_2)),
+                Tab(icon: Icon(Icons.other_houses)),
+                Tab(icon: Icon(Icons.calendar_month)),
               ],
             ),
-            title: const Text('Tabs Demo'),
           ),
           body: const TabBarView(
             children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
+              PeopleWidget(),
+              Icon(Icons.other_houses),
+              Icon(Icons.calendar_month),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class PeopleWidget extends StatelessWidget {
+  const PeopleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: ListBody(children: [
+      PersonWidget(name: 'A'),
+      PersonWidget(name: 'B'),
+      PersonWidget(name: 'C'),
+      PersonWidget(name: 'D'),
+    ]));
+  }
+}
+
+class PersonWidget extends StatelessWidget {
+  final String name;
+
+  const PersonWidget({Key? key, this.name = 'person'}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Person: ${name}'),
     );
   }
 }
