8cf6c56c440b77f7b2cb52e78dd407c925ccddfc
Tarsis Lima <br.tmvdl@gmail.com>
2024-12-03 23:09:31 -0300

    Updating app files

--- src/app/lib/main.dart
+++ src/app/lib/main.dart
@@ -1,57 +1,121 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const TabBarDemo());
-}
+void main() => runApp(const MyApp());
 
-class TabBarDemo extends StatelessWidget {
-  const TabBarDemo({super.key});
+static const appTitle = 'Vicentinos v0.1';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Vicentinos v0.1'),
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.person_2)),
-                Tab(icon: Icon(Icons.other_houses)),
-                Tab(icon: Icon(Icons.calendar_month)),
-              ],
-            ),
-          ),
-          body: const TabBarView(
-            children: [
-              PeopleWidget(),
-              Icon(Icons.other_houses),
-              Icon(Icons.calendar_month),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
     );
   }
 }
 
-class PeopleWidget extends StatelessWidget {
-  const PeopleWidget({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: ListView(children: [
-      PersonWidget(name: 'A'),
-      PersonWidget(name: 'B'),
-      PersonWidget(name: 'C'),
-      PersonWidget(name: 'D'),
-    ]));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Business'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('School'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
-}
-
-class PersonWidget extends ListTile {
-  const PersonWidget({String name = 'person', Key? key})
-      : super(key: key, title: const Text('$name'));
 }
